49268739dced24a7e1d4f10a3ae88476cdd8af07
nikitatihonov <email.address@example.net>
2015-09-25 02:40:05 +0300

    fix

--- Gitpard/urls.py
+++ Gitpard/urls.py
@@ -2,7 +2,9 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from Gitpard.views import time
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', time)
 ]
