10578f423703e8150fd58be2e002a2c1c0f0f833
Timur Safin <safin008@yandex.ru>
2015-10-01 15:45:48 +0300

    Реализация REST API

--- Gitpard/apps/repository/views.py
+++ Gitpard/apps/repository/views.py
@@ -1,6 +1,8 @@
 # coding: utf-8
+import datetime
 
 import os
+import shutil
 from urlparse import urlparse
 
 import git
@@ -31,28 +33,73 @@ class RepositoryViewSet(viewsets.ModelViewSet):
     def _clone_repo(self):
         """Клонирование репозитория"""
         obj = self.get_object()
+        status = ""
         if not (obj.state == Repository.NEW or
                 obj.state == Repository.FAIL_LOAD):
-            raise exceptions.NotFound
+            #raise exceptions.NotFound
+            status = u"Репозиторий уже был склонирован"
+            return status
         try:
             git.Repo.clone_from(obj.url, obj.path)
             obj.state = Repository.LOADED
+            status = u"Репозиторий успешно клонирован"
         except git.GitCommandError:
             if os.path.exists(obj.path):
                 os.rmdir(obj.path)
             obj.state = Repository.FAIL_LOAD
-            raise exceptions.ValidationError(
-                detail=u'Что-то пошло не так.Возможно урл репозитория'
-                       u'не действительный или есть проблемы с доступом')
+            #raise exceptions.ValidationError(
+            #    detail=u'Что-то пошло не так.Возможно урл репозитория'
+            #           u'не действительный или есть проблемы с доступом')
+            status = u"Ошибка при клонировании репозитория"
         finally:
             obj.last_modify = timezone.now()
             obj.save()
+            return status
+
+    def _update_repo(self):
+        #TODO реализовать обновление репозитория
+        return u"Репозиторий успешно обновлён"
+
+    def _delete_repo(self):
+        #TODO реализовать удаление папки с репозиторием
+        obj = self.get_object()
+        obj.delete()
+        return u"Репозиторий успешно удалён"
 
     @detail_route(methods=['get'])
     def clone(self, request, pk):
         """Rest метод запуска механизма клонирования репозитория."""
-        self._clone_repo()
-        return Response(status=status_codes.HTTP_200_OK)
+        status = self._clone_repo()
+        return Response({'status': status}, status=status_codes.HTTP_200_OK)
+
+    @detail_route(methods=['get'], url_path='update')
+    def update_repo(self, request, pk):
+        """Rest метод запуска механизма обновления репозитория."""
+        obj = self.get_object()
+        if not obj:
+            raise exceptions.NotFound
+        status = self._update_repo()
+        return Response({'status': status}, status=status_codes.HTTP_200_OK)
+
+    @detail_route(methods=['get', 'post'])
+    def edit(self, request, pk):
+        """Rest метод редактирования репозитория"""
+        obj = self.get_object()
+        if request.method == 'GET':
+            serializer = serializers.RepositorySerializer(obj)
+            return Response(serializer.data)
+        elif request.method == 'POST':
+            serializer = serializers.RepositorySerializer(obj, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'status': u"Данные сохранены"}, status=status_codes.HTTP_200_OK)
+            return Response(serializer.errors, status=status_codes.HTTP_400_BAD_REQUEST)
+
+    @detail_route(methods=['post'])
+    def delete(self, request, pk):
+        """Rest метод удаления репозитория"""
+        status = self._delete_repo()
+        return Response({'status': status}, status=status_codes.HTTP_204_NO_CONTENT)
 
     def get_queryset(self):
         return Repository.objects.filter(user=self.request.user)
