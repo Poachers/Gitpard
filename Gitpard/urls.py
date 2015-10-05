b8c737ea9245608dfd92fab5f4c27d0a2ffcf503
yellowdinar <kalimullindinrust@gmail.com>
2015-10-05 13:12:53 +0300

    УЖАААС

--- Gitpard/urls.py
+++ Gitpard/urls.py
@@ -3,8 +3,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from Gitpard.views import time
+from .api import get_api_urls
 
 urlpatterns = [
+    url(r'^api/', include(get_api_urls())),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', time)
+    url(r'^$', time)
 ]
