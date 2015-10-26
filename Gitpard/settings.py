7acf58a6e2bc6576d774506044a16f9368cd2cc9
yellowdinar <kalimullindinrust@gmail.com>
2015-10-26 08:23:25 +0300

    #29 Попытка запустить все на серваке, используя вместо redis, djcombu

--- Gitpard/settings.py
+++ Gitpard/settings.py
@@ -3,22 +3,9 @@ import os
 import djcelery
 djcelery.setup_loader()
 
-BROKER_HOST = "itis-demo.bars-open.ru"
-BROKER_BACKEND="redis"
-REDIS_PORT=80
-REDIS_HOST = "itis-demo.bars-open.ru"
-BROKER_USER = ""
-BROKER_PASSWORD =""
-BROKER_VHOST = "0"
-REDIS_DB = 0
-REDIS_CONNECT_RETRY = True
-CELERY_SEND_EVENTS=True
-CELERY_RESULT_BACKEND='redis'
-CELERY_TASK_RESULT_EXPIRES =  10
 CELERYBEAT_SCHEDULER="djcelery.schedulers.DatabaseScheduler"
 CELERY_ALWAYS_EAGER=False
-
-BROKER_URL = "redis://itis-demo.bars-open.ru:80/0"
+BROKER_BACKEND = "djkombu.transport.DatabaseTransport"
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -41,6 +28,7 @@ INSTALLED_APPS = (
     'social.apps.django_app.default',
     'Gitpard.apps.repository',
     'djcelery',
+    'djkombu',
 )
 
 
