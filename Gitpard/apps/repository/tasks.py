b050cc7ca634362992bd73cd2f0f5b369d875b82
yellowdinar <kalimullindinrust@gmail.com>
2015-11-08 10:39:54 +0300

    #40, #41 Добавление всех статусов, исправление ошибок с асинхронностью

--- Gitpard/apps/repository/tasks.py
+++ Gitpard/apps/repository/tasks.py
@@ -1,10 +1,22 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+import errno
 
 import git
 import django
 
 django.setup()
+import shutil
 
+import os
+import git
+import stat
+from rest_framework import viewsets, status as status_codes, exceptions
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+from django.utils import timezone
+from Gitpard.apps.repository import serializers
+from Gitpard.apps.repository.models import Repository
+from Gitpard.apps.repository.helpers import _get_url
 from celery import task
 from Gitpard.apps.repository.models import Repository
 from Gitpard.apps.repository.helpers import _get_url
@@ -12,31 +24,124 @@ from Gitpard.apps.repository.helpers import _get_url
 
 @task(ignore_result=True)
 def update(obj_id):
+    status = {}
     obj = Repository.objects.get(pk=obj_id)
-    repo = git.Repo.init(obj.path)
-    repo.git.fetch("origin")
-    repo.git.reset("--merge")
-    for ref in repo.remote("origin").refs:
-        if ref.remote_head == "HEAD":
-            continue
-        repo.git.checkout(ref.remote_head)
-        repo.git.pull("origin", ref.remote_head, v=True)
-    obj.state = Repository.LOADED
-    obj.save()
-    print 'Repo ' + str(obj.id) + ' update'
+    if not (obj.state == Repository.LOADED or obj.state == Repository.FAIL_UPDATE):
+        status["code"] = -1
+        status["message"] = u"Репозиторий не был склонирован"
+        return status
+    try:
+        obj.state = Repository.UPDATE
+        obj.save()
+        status["code"] = 5
+        status["message"] = u"Репозиторий обновляется"
+        repo = git.Repo.init(obj.path)
+        repo.git.fetch("origin")
+        repo.git.reset("--merge")
+        for ref in repo.remote("origin").refs:
+            if ref.remote_head == "HEAD":
+                continue
+            repo.git.checkout(ref.remote_head)
+            repo.git.pull("origin", ref.remote_head, v=True)
+        obj.state = Repository.LOADED
+        obj.save()
+        status["code"] = 1
+        status["message"] = u"Репозиторий обновлен"
+    except git.GitCommandError as e:
+        obj.state = Repository.FAIL_UPDATE
+        if str(e).find("not found") != -1:
+            status["code"] = -2
+            status["message"] = u"Репозиторий не найден"
+        elif str(e).find("Authentication failed") != -1:
+            status["code"] = -3
+            status["message"] = u"Ошибка авторизации"
+        else:
+            print "Update error: " + str(e)
+            status["code"] = -4
+            status["message"] = u'Ошибка сервера'
+    except Exception as e:
+        obj.state = Repository.FAIL_UPDATE
+        print "Update error: " + str(e)
+        status["code"] = -4
+        status["message"] = u'Ошибка сервера'
+    finally:
+        obj.last_modify = timezone.now()
+        obj.save()
+        return status
 
 
 @task(ignore_result=True)
 def clone(obj_id):
+    """Клонирование репозитория"""
     obj = Repository.objects.get(pk=obj_id)
-    git.Repo.clone_from(_get_url(obj), obj.path)
-    obj.state = Repository.LOADED
-    obj.save()
-    print 'Repo ' + str(obj.id) + ' clone'
-
+    status = {}
+    if not (obj.state == Repository.NEW or
+            obj.state == Repository.FAIL_LOAD):
+        status["code"] = -1
+        status["message"] = u"Репозиторий уже был склонирован"
+        return status
+    try:
+        obj.state = Repository.LOAD
+        obj.save()
+        status["code"] = 5
+        status["message"] = u"Репозиторий клонируется"
+        git.Repo.clone_from(_get_url(obj), obj.path)
+        obj.state = Repository.LOADED
+        obj.save()
+        status["code"] = 1
+        status["message"] = u"Репозиторий склонирован"
+    except git.GitCommandError as e:
+        if os.path.exists(obj.path):
+            shutil.rmtree(obj.path, ignore_errors=True)
+        obj.state = Repository.FAIL_LOAD
+        if str(e).find("not found") != -1:
+            status["code"] = -2
+            status["message"] = u"Репозиторий не найден"
+        elif str(e).find("Authentication failed") != -1:
+            status["code"] = -3
+            status["message"] = u"Ошибка авторизации"
+        else:
+            print "Clone error: " + str(e)
+            status["code"] = -4
+            status["message"] = u"Ошибка сервера"
+    except Exception as e:
+        if os.path.exists(obj.path):
+            shutil.rmtree(obj.path, ignore_errors=True)
+        obj.state = Repository.FAIL_LOAD
+        print "Clone error: " + str(e)
+        status["code"] = -4
+        status["message"] = u"Ошибка сервера"
+    finally:
+        obj.last_modify = timezone.now()
+        obj.save()
+        return status
 
 @task(ignore_result=True)
 def delete(obj_id):
     obj = Repository.objects.get(pk=obj_id)
+    """Удаление репозитория"""
+    status = {}
+    if not (obj.state == Repository.NEW or
+                    obj.state == Repository.LOADED or
+                    obj.state == Repository.FAIL_LOAD or
+                    obj.state == Repository.FAIL_UPDATE):
+        status["code"] = -1
+        status["message"] = u"Ведётся обработка репозитория, удаление невозможно"
+        return status
+
+    def handle_remove_readonly(func, path, exc):  # Удаляет файлы если было отказано в доступе
+        exc_value = exc[1]
+        if func in (os.rmdir, os.remove) and exc_value.errno == errno.EACCES:
+            os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+            func(path)
+        else:
+            raise
+    obj = Repository.objects.get(pk=obj_id)
+    if os.path.exists(obj.path):
+        status["code"] = 5
+        status["message"] = u"Репозиторий удаляется"
+        shutil.rmtree(obj.path, ignore_errors=False, onerror=handle_remove_readonly)
     obj.delete()
-    print 'Repo ' + str(obj.id) + ' delete'
+    status["code"] = 1
+    status["message"] = u"Репозиторий удален"
+    return status
--- Gitpard/apps/repository/views.py
+++ Gitpard/apps/repository/views.py
@@ -24,110 +24,27 @@ class RepositoryViewSet(viewsets.ModelViewSet):
         """Клонирование репозитория"""
         obj = self.get_object()
         status = {}
-        if not (obj.state == Repository.NEW or
-                obj.state == Repository.FAIL_LOAD):
-            status["code"] = -1
-            status["message"] = u"Репозиторий уже был склонирован"
-            return status
-        try:
-            obj.state = Repository.LOAD
-            obj.save()
-            """Асинхронный метод клонирования"""
-            clone.delay(obj.id)
-            status["code"] = 5
-            status["message"] = u"Репозиторий клонируется"
-        except git.GitCommandError as e:
-            if os.path.exists(obj.path):
-                shutil.rmtree(obj.path, ignore_errors=True)
-            obj.state = Repository.FAIL_LOAD
-            if str(e).find("not found") != -1:
-                status["code"] = -2
-                status["message"] = u"Репозиторий не найден"
-            elif str(e).find("Authentication failed") != -1:
-                status["code"] = -3
-                status["message"] = u"Ошибка авторизации"
-            else:
-                print "Clone error: " + str(e)
-                status["code"] = -4
-                status["message"] = u"Ошибка сервера"
-        except Exception as e:
-            if os.path.exists(obj.path):
-                shutil.rmtree(obj.path, ignore_errors=True)
-            obj.state = Repository.FAIL_LOAD
-            print "Clone error: " + str(e)
-            status["code"] = -4
-            status["message"] = u"Ошибка сервера"
-        finally:
-            obj.last_modify = timezone.now()
-            obj.save()
-            return status
+        clone.delay(obj.id)
+        status["code"] = 7
+        status["message"] = u"Подготовка к клонированию"
+        return status
 
     def _update_repo(self):
         """Обновление репозитория"""
         obj = self.get_object()
         status = {}
-        if not (obj.state == Repository.LOADED or
-                obj.state == Repository.FAIL_UPDATE):
-            status["code"] = -1
-            status["message"] = u"Репозиторий не был склонирован"
-            return status
-        try:
-            obj.state = Repository.UPDATE
-            obj.save()
-            """Асинхронный метод обновления"""
-            update.delay(obj.id)
-            status["code"] = 5
-            status["message"] = u"Репозиторий обновляется"
-        except git.GitCommandError as e:
-            obj.state = Repository.FAIL_UPDATE
-            if str(e).find("not found") != -1:
-                status["code"] = -2
-                status["message"] = u"Репозиторий не найден"
-            elif str(e).find("Authentication failed") != -1:
-                status["code"] = -3
-                status["message"] = u"Ошибка авторизации"
-            else:
-                print "Update error: " + str(e)
-                status["code"] = -4
-                status["message"] = u"Ошибка сервера"
-        except Exception as e:
-            obj.state = Repository.FAIL_UPDATE
-            print "Update error: " + str(e)
-            status["code"] = -4
-            status["message"] = u"Ошибка сервера"
-        finally:
-            obj.last_modify = timezone.now()
-            obj.save()
-            return status
+        update.delay(obj.id)
+        status["code"] = 7
+        status["message"] = u"Подготовка к обновлению"
+        return status
 
     def _delete_repo(self):
-        """Удаление репозитория"""
         obj = self.get_object()
         status = {}
-        if not (obj.state == Repository.NEW or
-                obj.state == Repository.LOADED or
-                obj.state == Repository.FAIL_LOAD or
-                obj.state == Repository.FAIL_UPDATE):
-            status["code"] = -1
-            status["message"] = u"Ведётся обработка репозитория, удаление невозможно"
-            return status
-
-        def handle_remove_readonly(func, path, exc):  # Удаляет файлы если было отказано в доступе
-            exc_value = exc[1]
-            if func in (os.rmdir, os.remove) and exc_value.errno == errno.EACCES:
-                os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-                func(path)
-            else:
-                raise
-
-        obj = self.get_object()
-        if os.path.exists(obj.path):
-            shutil.rmtree(obj.path, ignore_errors=False, onerror=handle_remove_readonly)
         """Асинхронный метод удаления"""
         delete.delay(obj.id)
-        status["code"] = 5
-        status["message"] = u"Репозиторий удаляется"
-        return status
+        status["code"] = 7
+        status["message"] = u"Подготовка к удалению"
 
     @detail_route(methods=['get'])
     def clone(self, request, pk):
