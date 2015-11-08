c999217b72b7e6d9e56a8a337049487b644a5574
nikitatihonov <email.address@example.net>
2015-11-08 12:11:08 +0300

    #35 Время Front

--- Gitpard/views.py
+++ Gitpard/views.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from django.contrib.auth.decorators import login_required
-
+from django.conf import settings
+from django.core.files import File
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
 
 
@@ -12,7 +14,13 @@ def time(request):
 
 @login_required(login_url='/index')
 def analysis(request):
-    return render(request, 'analysis.html')
+    file_html = open(settings.BASE_DIR + '/Gitpard/apps/repository/views.py')
+    file = []
+    for i in file_html.readlines():
+        file.append(dict(num=len(file), title=i, a='guy-full-in', date='4.10.2015', com='fix fix fix fix fix fix fix fix fix'))
+    file_html.close()
+    return render_to_response('analysis.html', {'file': file})
+    # return render(request, 'analysis.html')
 
 
 def index(request):
