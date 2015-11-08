fbb3754ef75c1944ad8c450b1fdef73789cae1cf
yellowdinar <kalimullindinrust@gmail.com>
2015-11-08 14:48:47 +0300

    #54 Теперь пользователь ограничен в 3 одновременно выполняющихся асинхронных задачах

--- Gitpard/apps/repository/views.py
+++ Gitpard/apps/repository/views.py
@@ -1,50 +1,71 @@
 # coding: utf-8
-import errno
-import shutil
 
 import os
+
 import git
-import stat
-from django.http import Http404
 from rest_framework import viewsets, status as status_codes, exceptions
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
-from django.utils import timezone
+from django.contrib.auth.models import User
 from Gitpard.apps.repository import serializers
 from Gitpard.apps.repository.models import Repository
-from Gitpard.apps.repository.helpers import _get_url, get_pure_url
+from Gitpard.apps.repository.helpers import _get_url
 from Gitpard.apps.repository.tasks import clone, update, delete
+
 
 class RepositoryViewSet(viewsets.ModelViewSet):
     """Viewset на основе сериализатора модели репозитория."""
     serializer_class = serializers.RepositorySerializer
     queryset = Repository.objects
+    async_methods = 3
+
+    def check_repos_in_celery(self):
+        """Количество запущенных клонирований и обновлений."""
+        user_repositeries = Repository.objects.filter(user=self.request.user)
+        repo_in_celery = 0
+        for repo in user_repositeries:
+            if repo.state == 3 or repo.state == 1:
+                repo_in_celery += 1
+        return repo_in_celery
 
     def _clone_repo(self):
         """Клонирование репозитория"""
-        obj = self.get_object()
         status = {}
-        clone.delay(obj.id)
-        status["code"] = 7
-        status["message"] = u"Подготовка к клонированию"
+        if self.check_repos_in_celery() < self.async_methods:
+            obj = self.get_object()
+            clone.delay(obj.id)
+            status["code"] = 7
+            status["message"] = u"Подготовка к клонированию"
+        else:
+            status["code"] = 9
+            status["message"] = u"Уже запущено " + str(self.async_methods) + u" асихронных задания"
         return status
 
     def _update_repo(self):
         """Обновление репозитория"""
-        obj = self.get_object()
         status = {}
-        update.delay(obj.id)
-        status["code"] = 7
-        status["message"] = u"Подготовка к обновлению"
+        if self.check_repos_in_celery() < self.async_methods:
+            obj = self.get_object()
+            update.delay(obj.id)
+            status["code"] = 7
+            status["message"] = u"Подготовка к обновлению"
+        else:
+            status["code"] = 9
+            status["message"] = u"Уже запущено " + str(self.async_methods) + u" асихронных задания"
         return status
 
     def _delete_repo(self):
-        obj = self.get_object()
         status = {}
-        """Асинхронный метод удаления"""
-        delete.delay(obj.id)
-        status["code"] = 7
-        status["message"] = u"Подготовка к удалению"
+        if self.check_repos_in_celery() < self.async_methods:
+            obj = self.get_object()
+            """Асинхронный метод удаления"""
+            delete.delay(obj.id)
+            status["code"] = 7
+            status["message"] = u"Подготовка к удалению"
+        else:
+            status["code"] = 9
+            status["message"] = u"Уже запущено " + str(self.async_methods) + u" асихронных задания"
+        return status
 
     @detail_route(methods=['get'])
     def clone(self, request, pk):
